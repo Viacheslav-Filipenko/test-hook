99f45a0100f8329589423dee1e32083386c89bbc
Viacheslav-Filipenko <viacheslav.filipenko@smartexe.com>
2021-01-26 14:14:41 +0200

    hot fix

--- src/modules/carousel/components/CarouselHeadline/carousel-headline.jss.ts
+++ src/modules/carousel/components/CarouselHeadline/carousel-headline.jss.ts
@@ -1,35 +1,33 @@
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((_theme: Theme) => {
-   return createStyles({
-      carouselHeadline: {
-         '& $headline': {
-            paddingBottom: 24,
-            '& hr': {
-               marginBottom: 21
-            }
-         },
-         '& $headlineWrapper': {
-            width: '100%',
-            maxWidth: 1280
-         },
-         '& $title': {
-            position: 'relative',
-            fontSize: '21px'
-         },
-         '& $divider': {
-            position: 'relative',
-            height: 3
-         },
-         '& $link': { '&:focus': { textDecoration: 'underline' } },
-         '& $container': {
-            display: 'flex',
-            margin: '0 auto',
-            maxWidth: 1280,
-            justifyContent: 'center'
+const useStyles = makeStyles(
+   createStyles({
+      headline: {
+         paddingBottom: 24,
+         '& hr': {
+            marginBottom: 21
          }
+      },
+      headlineWrapper: {
+         width: '100%',
+         maxWidth: 1280
+      },
+      title: {
+         position: 'relative',
+         fontSize: '21px'
+      },
+      divider: {
+         position: 'relative',
+         height: 3
+      },
+      link: { '&:focus': { textDecoration: 'underline' } },
+      container: {
+         display: 'flex',
+         margin: '0 auto',
+         maxWidth: 1280,
+         justifyContent: 'center'
       }
-   });
-});
+   })
+);
 
 export default useStyles;
