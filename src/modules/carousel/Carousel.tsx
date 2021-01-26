fe423e5fd6f51db835dd6265c65568e6385b3053
Viacheslav-Filipenko <viacheslav.filipenko@smartexe.com>
2021-01-26 17:42:51 +0200

    refactor carousel

--- src/modules/carousel/Carousel.tsx
+++ src/modules/carousel/Carousel.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
 
-
-import CarouselProvider from 'nuka-carousel';
+import CarouselProvider, { CarouselProps } from 'nuka-carousel';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
-
-import {
-   CollectionCard,
-   ICollectionCardProps
-} from '../collection/components/CollectionCard/CollectionCard';
-import {
-   ICarouselImageCardProps,
-   CarouselImageCard
-} from './components/CarouselImageCard/CarouselImageCard';
-
 
 import withWidth from '@material-ui/core/withWidth';
 import { withStylesInjectedFirst } from '../../core/hocs/with-styles-injected-first';
@@ -25,44 +14,21 @@ interface ISizes {
    sm: number;
    lg: number;
 }
-export interface ICarouselProps {
+export interface ICarouselProps extends CarouselProps {
    //color
    arrowColor?: string;
-   childrenColor?: string;
 
-   // bool flags
-   arrows?: boolean;
-   autoplay?: boolean;
-
-   // slide params
-   currenSlide?: number;
-   sliderClasses?: '';
-   isCardImage: boolean;
-   infinite: boolean;
    width: string;
    visibleSlides: ISizes;
-   cellSpacing?: number;
-   data: (ICarouselImageCardProps | ICollectionCardProps)[];
+
+   children?: React.ReactNode;
+
+   data: any[];
 }
 
-const mapSlides = (data: any[], isCard: boolean) => {
-   return isCard
-      ? data.map((itm, i) => <CarouselImageCard key={i} {...itm} />)
-      : data.map((itm, i) => (
-           <CollectionCard
-              tintColor={itm.tintColor}
-              onClick={() => itm.onClick()}
-              count={itm.elementCount}
-              title={itm.title}
-              image={itm.image}
-              subtitle={itm.subtitle}
-              key={i}
-           />
-        ));
-};
 
 const CarouselComponent: React.FC<ICarouselProps> = (props) => {
-   const { arrowColor = 'black', width, isCardImage, visibleSlides, cellSpacing, data } = props;
+   const { arrowColor = 'black', cellSpacing, width, visibleSlides, children, data } = props;
 
    const classes = useStyles();
 
@@ -72,19 +38,6 @@ const CarouselComponent: React.FC<ICarouselProps> = (props) => {
    return (
       <div className={classes.sliderContainer}>
          <CarouselProvider
-            getControlsContainerStyles={(key) => {
-               if (!isCardImage) return {};
-               switch (key) {
-                  case 'CenterLeft':
-                     return {
-                        top: '100px'
-                     };
-                  default:
-                     return {
-                        top: '100px'
-                     };
-               }
-            }}
             dragging={true}
             enableKeyboardControls={true}
             renderBottomCenterControls={() => ''}
@@ -92,7 +45,7 @@ const CarouselComponent: React.FC<ICarouselProps> = (props) => {
             disableEdgeSwiping={true}
             cellSpacing={cellSpacing}
             renderCenterLeftControls={({ previousSlide, currentSlide }) => {
-               if (_visibleSlides >= data.length || currentSlide === 0) return <div />;
+               if (_visibleSlides >= data?.length || currentSlide === 0) return <div />;
                return (
                   <button
                      arial-label="previous topic"
@@ -119,8 +72,10 @@ const CarouselComponent: React.FC<ICarouselProps> = (props) => {
             autoplay={true}
             slidesToScroll={1}
             cellAlign={'left'}
-            slidesToShow={_visibleSlides}>
-            {mapSlides(data, isCardImage)}
+            slidesToShow={_visibleSlides}
+            {...props}
+            >
+            {children}
          </CarouselProvider>
       </div>
    );
